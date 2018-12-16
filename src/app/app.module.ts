41738338c6464fb40194ffdd104597e8f0e00fae
somendra pratap singh <somenprtap.singh@gmail.com>
2018-12-16 16:28:15 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesComponent } from './services/services.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
